41d8d07c07ee7251c95c1d8741137f6352d2d4ef
Juan José Cascante López <jcascante@aseccss.com>
2025-09-11 17:02:19 -0600

    Agrego nuevo datos para ejecutar la prueba de login

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: "o6isn5", // Id de Cypress Cloud
 
+  reporter: "mochawesome",   // Reporte a utilizar en el proyecto
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true, // true para cuando necesitamos 1, false para poder consolidar el reporte
+    json: true //true para poder tomar de cada 1 y unificarlo
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // Opciones de configuración aquí
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,15 +7,16 @@ describe('Login en SauceDemo con múltiples usuarios', () => {
   const usuarios = require('../fixtures/usuarios.json')
 
   usuarios.forEach((usuario) => {
-    it(`Debería probar login con: ${usuario.username}`, () => {
+    it('Vamos a probar login con el usuario:' + usuario.username, () => {
       cy.get('#user-name').clear().type(usuario.username)
       cy.get('#password').clear().type(usuario.password)
       cy.get('#login-button').click()
 
-      if (usuario.expected === "success") {
+      if (usuario.resultado === "exitoso") {
         cy.url().should('include', '/inventory.html')
         cy.contains('Products').should('be.visible')
       } else {
+        cy.url().should('include', 'https://www.saucedemo.com/')
         cy.get('[data-test="error"]').should('be.visible')
       }
     })
